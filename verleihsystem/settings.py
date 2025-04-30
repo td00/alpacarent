1872b7dfc20929f65dd8b070aef4b237e955c3d3
Thies Mueller <>
2025-04-30 13:16:05 +0200

    add default secret

--- verleihsystem/settings.py
+++ verleihsystem/settings.py
@@ -14,7 +14,7 @@ from decouple import config, Csv
 from pathlib import Path
 
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-yb&yqhylx6o7o3f$6-=3zxoxyxftkb2+di%5@8js&+q!b)28st")
 DEBUG = config("DEBUG", cast=bool, default=False)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv, default="localhost")
 TIME_ZONE = config("TIME_ZONE", default="Europe/Berlin")
