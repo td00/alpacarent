fda5dc3234a0f48f0ed8d69fa0e45a9d60c76b3f
Thies Mueller <>
2025-04-30 13:14:30 +0200

    add env vars for settings

--- verleihsystem/settings.py
+++ verleihsystem/settings.py
@@ -10,7 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import config, Csv
 from pathlib import Path
+
+
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", cast=bool, default=False)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv, default="localhost")
+TIME_ZONE = config("TIME_ZONE", default="Europe/Berlin")
+LANGUAGE_CODE = config("LANGUAGE_CODE", default="de-DE")
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,14 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yb&yqhylx6o7o3f$6-=3zxoxyxftkb2+di%5@8js&+q!b)28st"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 
 ROOT_URLCONF = 'verleihsystem.urls'
 
@@ -105,9 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "de-DE"
-
-TIME_ZONE = "Europe/Berlin"
 
 USE_I18N = True
 
